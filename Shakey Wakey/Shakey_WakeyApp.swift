f0ca414101f003fe75eb5a210ae89bd853880420
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-04-12 12:37:04 +0200

    Initial Commit

---
+++ Shakey Wakey/Shakey_WakeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shakey_WakeyApp.swift
+//  Shakey Wakey
+//
+//  Created by Johan Karlsson on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct Shakey_WakeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shakey WakeyTests/Shakey_WakeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shakey_WakeyTests.swift
+//  Shakey WakeyTests
+//
+//  Created by Johan Karlsson on 2025-04-12.
+//
+
+import Testing
+@testable import Shakey_Wakey
+
+struct Shakey_WakeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
